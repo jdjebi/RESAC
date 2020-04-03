203b9e143128053a3cecce8ec5c98686fa6ac2de
jdjebi <jeanmarcdjebi@gmail.com>
2020-04-03 23:33:01 +0000

    Correctif

--- api/user/new.php
+++ api/user/new.php
@@ -1,8 +1,44 @@
 <?php
 
+require __DIR__."/../../src/init.php";
+
 $data = [
   'test' => "test"
 ];
 
-echo json_encode($data);
+$form = new RegisterForm($_POST);
+
+if($_POST){
+  // Validation
+  if($form->is_validate()){
+
+    // Enregistrement
+    $data = $form->get_data();
+
+    /*
+    $data = [
+      "nom" => $data["nom"],
+      "prenom" => $data["prenom"],
+      "email" => $data["email"],
+      "password" => crypt_password($data["password"])
+    ];
+
+    $user = Users::create($data);
+
+    // Notification
+    // Flash
+    echo "Inscription réussie. Vous pouvez vous connecter.";
+    */
+
+    // Redirection
+    echo "Success";
+    // Redirect::route('home');
+  }else{
+    // Renvoye des erreurs
+    $errors = $form->get_errors();
+
+    var_dump($errors);
+  }
+}
+
 ?>
