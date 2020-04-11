2ffdea80a86ef0915cb523e5b418c6a682a24436
jdjebi <jeanmarcdjebi@gmail.com>
2020-04-11 08:21:59 +0000

    Correctif

--- config/db.php
+++ config/db.php
@@ -1,10 +1,19 @@
 <?php
 
+var_dump($_SERVER);
+
 return [
   "host" => "localhost",
   "username" => "root",
   "password" => "",
   "dbname" => "youniti"
 ];
+
+return [
+  "host" => "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  "username" => "nc0p25hz6kjx2boe",
+  "password" => "y6t93gnem2pphg3z",
+  "dbname" => "qn4vudjk5am7incz"
+]
 
 ?>
