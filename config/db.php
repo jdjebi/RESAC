589868cbaad7dc537b63b288eccff304c480cf47
jdjebi <jeanmarcdjebi@gmail.com>
2020-04-11 08:27:13 +0000

    Correctif

--- config/db.php
+++ config/db.php
@@ -1,19 +1,24 @@
 <?php
 
-var_dump($_SERVER);
+if(isset($_SERVER["HEROKU_APP_DIR"])){
+  
+  return [
+    "host" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "dbname" => "youniti"
+  ];
 
-return [
-  "host" => "localhost",
-  "username" => "root",
-  "password" => "",
-  "dbname" => "youniti"
-];
+}else{
 
-return [
-  "host" => "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-  "username" => "nc0p25hz6kjx2boe",
-  "password" => "y6t93gnem2pphg3z",
-  "dbname" => "qn4vudjk5am7incz"
-]
+  return [
+    "host" => "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    "username" => "nc0p25hz6kjx2boe",
+    "password" => "y6t93gnem2pphg3z",
+    "dbname" => "qn4vudjk5am7incz"
+  ];
+
+}
+
 
 ?>
