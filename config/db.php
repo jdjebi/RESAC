e4c8d3f7a2fc7eb11ff23fff194931a1208f1a43
jdjebi <jeanmarcdjebi@gmail.com>
2020-04-13 23:36:07 +0000

    Correction du nom de l'hote de la base de données

--- config/db.php
+++ config/db.php
@@ -16,7 +16,7 @@ if(!isset($_SERVER["HEROKU_APP_DIR"])){
   $DEBUG = true;
 
   return [
-    "host" => "gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com	",
+    "host" => "gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
     "username" => "vipft88ee325qrcg",
     "password" => "rvkqpzrk0ei8pzt3",
     "dbname" => "n9syu0b9t9ougqmw"
