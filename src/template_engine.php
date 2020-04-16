63a0aa72ddd13b22ee67ae4e4cb2b0fbb3ecd969
jdjebi <jeanmarcdjebi@gmail.com>
2020-04-16 18:22:52 +0000

    Mise à jour

--- src/template_engine.php
+++ src/template_engine.php
@@ -2,7 +2,7 @@
 
 use Jenssegers\Blade\Blade;
 
-$template_engine_renderer = new Blade('views/blade', 'cache');
+$template_engine_renderer = new Blade('views/blade', 'storage/cache');
 
 function render($view_name,$context=[]){
   global $template_engine_renderer;
