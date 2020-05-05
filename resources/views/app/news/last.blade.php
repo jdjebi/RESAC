73ce9102e58c1430de2b10127e99fe4d8e6e6bd0
jdjebi <jeanmarcdjebi@gmail.com>
2020-05-05 12:48:18 +0000

    Mise à jour mineure

--- resources/views/app/news/last.blade.php
+++ resources/views/app/news/last.blade.php
@@ -4,14 +4,13 @@
 		<div class="col-md-6 offset-md-3">
 			<h4>Les nouveautés</h4>
 			<ul class="timeline">
-        <li>
-          <span class="text-primary">Page d'actualités première version</span>
-					<div class="text-muted small"><i class="far fa-clock"></i> 20 Avril 2020</div>
+				<li>
+					<span class="text-primary"><b>Certification des publications</b></span>
+					<div class="text-muted small"><i class="far fa-clock"></i> 5 Mai 2020</div>
 					<p class="mt-2">
-            Il est désormais possible de publier du contenu textuelle. Il s'agit de la première version de la fil d'actualités destinée à permettre aux membres de RESAC d'interagir au travers de publications textuelle.
-            Pour l'instant, la fil se contente d'afficher les publications des plus récentes aux plus anciennes. Par ailleurs des fonctionnalités mineures seront intégrées afin d'améliorer l'exprérience utilisateur.
-          </p>
-        </li>
+						Désormais, lorsqu'un contenu est publié, il est marqué du badge <i class=" text-danger fa fa-check-circle"></i> indiquant qu'il n'a pas été approuvé. En effet, pour garantir la pertinence du contenu publié, toutes les publications devront être approuvée par les administrateurs de RESAC. Ainsi, tout contenu approuvé sera marqué du badge <i class=" text-success fa fa-check-circle"></i>. L'objectif étant de faire la distinction entre les contenus jugé pertinent et non mais aussi de limiter la publication de contenu inaproprié.
+					</p>
+				</li>
 			</ul>
 		</div>
     <div class="col-md-6 offset-md-3">
--- resources/views/app/news/timeline.blade.php
+++ resources/views/app/news/timeline.blade.php
@@ -10,6 +10,13 @@
 			<h4>Fil des nouveautés</h4>
 			<ul class="timeline">
 				<li>
+					<span class="text-primary"><b>Certification des publications</b></span>
+					<div class="text-muted small"><i class="far fa-clock"></i> 5 Mai 2020</div>
+					<p class="mt-2">
+						Désormais, lorsqu'un contenu est publié, il est marqué du badge <i class=" text-danger fa fa-check-circle"></i> indiquant qu'il n'a pas été approuvé. En effet, pour garantir la pertinence du contenu publié, toutes les publications devront être approuvée par les administrateurs de RESAC. Ainsi, tout contenu approuvé sera marqué du badge <i class=" text-success fa fa-check-circle"></i>. L'objectif étant de faire la distinction entre les contenus jugé pertinent et non mais aussi d'éviter la publication de contenu inaproprié.
+					</p>
+				</li>
+				<li>
 					<span class="text-primary"><b>Page d'actualités première version</b></span>
 					<div class="text-muted small"><i class="far fa-clock"></i> 20 Avril 2020</div>
 					<p class="mt-2">
