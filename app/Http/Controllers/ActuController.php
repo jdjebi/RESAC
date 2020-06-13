d8c9bc5421140d8dce2da9fe233a7109f83ee13a
jdjebi <jeanmarcdjebi@gmail.com>
2020-06-13 11:00:50 +0000

    Correctifs

--- app/Http/Controllers/ActuController.php
+++ app/Http/Controllers/ActuController.php
@@ -76,8 +76,6 @@ class ActuController extends Controller
 
       dump($user->photo);
 
-      dump(asset("asset/imgs/user_default_pic.png"));
-
       return view("app.actu",[
         'title' => $title,
         'user' => $user,
