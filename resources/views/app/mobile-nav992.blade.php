d83bfb4945c52ddf2ff2113d856a21ced841b8a2
jdjebi <jeanmarcdjebi@gmail.com>
2020-08-18 19:59:21 +0000

    Correctif

--- resources/views/app/mobile-nav992.blade.php
+++ resources/views/app/mobile-nav992.blade.php
@@ -3,24 +3,24 @@
       <button id="home" type="button" class="btn btn-secondary button-active">
          <div>
            <a class="selector-holder" href="{{ route('actu') }}">
-             <i class="far fa-2x fa-newspaper"></i>
-             <span class="d-none d-sm-block">Actualités</span>
+             <i class="far fa-newspaper"></i>
+             <span>Actualités</span>
            </a>
          </div>
       </button>
       <button id="feed" type="button" class="btn btn-secondary">
         <div>
           <a class="selector-holder" href="{{ route('annuaire') }}">
-            <i class="fa fa-2x fa-address-book"></i>
-            <span class="d-none d-sm-block">Annuaire</span>
+            <i class="fa fa-address-book"></i>
+            <span>Annuaire</span>
           </a>
         </div>
       </button>
       <button id="create" type="button" class="btn btn-secondary button-inactive">
         <div>
           <a class="selector-holder" href="{{ route('profil') }}">
-            <i class="fa fa-2x fa-user-circle"></i>
-            <span class="d-none d-sm-block">Mon Profil</span>
+            <i class="fa fa-user-circle"></i>
+            <span>Mon Profil</span>
           </a>
         </div>
       </button>
@@ -28,8 +28,8 @@
         <button id="account" type="button" class="btn btn-secondary button-inactive">
            <div>
              <a class="selector-holder" href="{{ route('admin_user_manager') }}">
-               <i class="fa fa-2x fa-tachometer-alt"></i>
-               <span class="d-none d-sm-block">Administration</span>
+               <i class="fa fa-tachometer-alt"></i>
+               <span>Administration</span>
              </a>
            </div>
         </button>
