0c6ff68b8044ac664b72c2c78246615ef93401c6
jdjebi <jeanmarcdjebi@gmail.com>
2020-10-17 22:30:48 +0000

    Correctif

--- app/Http/Resources/Post/PostCollection.php
+++ app/Http/Resources/Post/PostCollection.php
@@ -14,53 +14,57 @@ class PostCollection extends ResourceCollection
 
         $posts = [];
 
-        $user = null;
+        if($posts_tmp){
 
-        foreach ($posts_tmp as $post) {
+            foreach ($posts_tmp as $post) {
 
-            $user = User::find($post->user_object->id);
+                $user = User::find($post->user_object->id);
 
-            dump($user);
+                $posts[] = [
+                    'id' => $post->id,
+                    'user_id' => $post->user_object->id,
+                    'scope' => $post->scope,
+                    'type' => $post->type,
+                    'date' => $post->date,
+                    'content' => trim($post->content),
+                    'version' => $post->version,
+                    'validate' => $post->validate,
+                    'validate_by' => $post->validate_by, 
+                    'validate_at' => $post->validate_at,
+                    'validate_status' => $post->validate_status,
+                    'status'  => $post->status,
+                    'is_active' => $post->is_active,
+                    'is_published' => $post->is_published,
+                    'validate_status_title' => $post->validate_status_title,
+                    'validate_status_tag' => $post->validate_status_tag   
+                ];
+            }
 
-            die();
+            return [
+                "posts" => $posts,
+                "user" => [
+                    'id' => $user->id,
+                    'nom' => $user->nom,
+                    'prenom' => $user->prenom,
+                    'fullname' => $user->fullname,
+                    'promo' => $user->promo,
+                    'universite' => $user->universite,
+                    'emploi' => $user->emploi,
+                    'pays' => $user->pays,
+                    'photo' => photos_cdn_asset($user), 
+                    'ville' => $user->ville,
+                    'commune' => $user->commune,
+                    'numero' => $user->numero,
+                    'admin_profil_url' => route('admin_user_profil',['user_id' => $user->id]),
+                ],
+            ];
 
-            $posts[] = [
-                'id' => $post->id,
-                'user_id' => $post->user_object->id,
-                'scope' => $post->scope,
-                'type' => $post->type,
-                'date' => $post->date,
-                'content' => trim($post->content),
-                'version' => $post->version,
-                'validate' => $post->validate,
-                'validate_by' => $post->validate_by, 
-                'validate_at' => $post->validate_at,
-                'validate_status' => $post->validate_status,
-                'status'  => $post->status,
-                'is_active' => $post->is_active,
-                'is_published' => $post->is_published,
-                'validate_status_title' => $post->validate_status_title,
-                'validate_status_tag' => $post->validate_status_tag   
+        } else{
+            return [
+                "posts" => [],
+                "user" => null
             ];
         }
 
-        return [
-            "posts" => $posts,
-            "user" => [
-                'id' => $user->id,
-                'nom' => $user->nom,
-                'prenom' => $user->prenom,
-                'fullname' => $user->fullname,
-                'promo' => $user->promo,
-                'universite' => $user->universite,
-                'emploi' => $user->emploi,
-                'pays' => $user->pays,
-                'photo' => photos_cdn_asset($user), 
-                'ville' => $user->ville,
-                'commune' => $user->commune,
-                'numero' => $user->numero,
-                'admin_profil_url' => route('admin_user_profil',['user_id' => $user->id]),
-            ],
-        ];
     }
 }
