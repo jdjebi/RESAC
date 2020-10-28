403fa7b93eaa6d9a545a642ee533b9f2258d8a06
jdjebi <jeanmarcdjebi@gmail.com>
2020-10-28 14:23:25 +0000

    Ajout de spatie/permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Post;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
 
     protected $table = 'users';
 
