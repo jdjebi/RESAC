165e33b6cc6e7bf87ee4839ce2bfd643ee73fe32
jdjebi <jeanmarcdjebi@gmail.com>
2021-04-05 09:51:31 +0000

    Création d'un seeder pour l'association des rôles et permissions

--- database/seeds/RolePermissionSeeder.php
+++ database/seeds/RolePermissionSeeder.php
@@ -1,16 +1,26 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\RESAC\Core\Security\RolesFactory;
 
 class RolePermissionSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Association des roles et permissions
      *
      * @return void
      */
     public function run()
     {
-        //
+        $roles = RolesFactory::ROLES_DATABASE;
+
+        foreach ($roles as $role) {
+
+            $roleModel = Role::where('name',$role['name'])->where('guard_name',$role['guard'])->first();
+
+            $roleModel->syncPermissions($role["permissions"]);
+        
+        }
     }
 }
