20bd2bd39a45c88a8b3a894efae32084446570aa
jdjebi <jeanmarcdjebi@gmail.com>
2021-04-05 09:46:47 +0000

    Ajout des permissions

--- app/Resac/Core/Security/RolesFactory.php
+++ app/Resac/Core/Security/RolesFactory.php
@@ -7,18 +7,22 @@ class RolesFactory{
     // Permissions
     const PERMISSIONS_DATABASE = [
         "dev-tools-access" => [
+            "name" => "dev-tools-access",
             "label" => "Accès outils développeur",
             "description" => "Création de notifications; Manipulation de l'index de recherche; Manipulation des mlessages flash"
         ],
         "news-manage" => [
+            "name" => "news-manage",
             "label" => "Gestion des nouveautés",
             "description" => "Créer, modifier, affichage, suppression des nouveautés"
         ],
         "post-manage" => [
+            "name" => "post-manage",
             "label" => "Gestion des publications",
             "description" => "Autorise la gestion des publications et l'interaction avec le système de publicaton"
         ],
         "user-manage" => [
+            "name" => "user-manage",
             "label" => "Gestion des utilisateurs",
             "description" => "Autorise la suppression des membres et modérateurs et la manipulation des rôles (Attribution du rôle administrateur est réservé au super admin)"
         ]
@@ -31,30 +35,36 @@ class RolesFactory{
             "label" => "Super Administrateur",
             "level" => 5,
             "guard"  => 'web',
+            "permissions" => []
         ],
         "admin" => [
             "name" => "admin",
             "label" => "Administrateur",
             "level" => 4,
             "guard"  => 'web',
+            "permissions" => ["post-manage","user-manage","news-manage"]
+
         ],
         "developer" => [
             "name" => "developer",
             "label" => "Développeur",
             "level" => 3,
-            "guard"  => 'web',
+            "guard" => 'web',
+            "permissions" => ["dev-tools-access", "news-manage"]
         ],
         "moderator" => [
             "name" => "moderator",
             "label" => "Modérateur",
             "level" => 2,
             "guard"  => 'web',
+            "permissions" => ["dev-tools-access", "post-manage"]
         ],
         "member" => [
             "name" => "member",
             "label" => "Membre",
             "level" => 1,
             "guard"  => 'web',
+            "permissions" => []
         ],
     ];
 
