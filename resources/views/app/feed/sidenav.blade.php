6449c9356ea4d93ea536e06bf64d943b55b4f63e
jdjebi <jeanmarcdjebi@gmail.com>
2021-04-07 15:02:51 +0000

    Ajout d'un lien de création de publication

--- resources/views/app/feed/sidenav.blade.php
+++ resources/views/app/feed/sidenav.blade.php
@@ -2,4 +2,9 @@
     <li class="nav-item nav-pills">
         <a class='nav-link small' href="{{ route('app.suggestions.all') }}">Suggestions</a>
     </li>
+    @can("post-manage")
+    <li class="nav-item nav-pills">
+        <a class='nav-link small' href="{{ route('admin.post.create.libre') }}">Créer une publication</a>
+    </li>
+    @endcan
 </ul>
