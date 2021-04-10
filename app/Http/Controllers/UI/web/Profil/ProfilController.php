cd627c85d98b1f2f12f2e366fc46bbf0332b0412
jdjebi <jeanmarcdjebi@gmail.com>
2021-04-10 23:15:28 +0000

    Mise à jour de backup

--- app/Http/Controllers/UI/web/Profil/ProfilController.php
+++ app/Http/Controllers/UI/web/Profil/ProfilController.php
@@ -33,7 +33,9 @@ class ProfilController extends Controller
           $title =  $user_visited->nom.' '.$user_visited->prenom;
         }
 
-        $posts = Post::where("user",$user->id)->orderBy('date', 'desc')->get();
+        $title =  $user_visited->nom.' '.$user_visited->prenom;
+
+        $posts = Post::where("user_id",$user_visited->id)->orderBy('date', 'desc')->get();
 
         $posts = PostRenderer::render_posts($posts);
 
