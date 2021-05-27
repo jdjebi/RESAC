3eb2730d105b1645141809caff1acaca1d93dab2
jdjebi <39745200+jdjebi@users.noreply.github.com>
2021-05-27 20:05:52 +0000

    Ajout de l'email des administrateurs

--- config/var.php
+++ config/var.php
@@ -9,6 +9,7 @@ return [
     |
     |
     */
+    'resac_email' => 'contactresacd@gmail.com',
     'user_default_photo' => "asset/imgs/user_default_pic.png",
     'database_connection_failed' => "\\..\\database\\failed.php" // Chemin vers le fichier d'erreur de connexion à mysql depuis AppServiceProvider
 
