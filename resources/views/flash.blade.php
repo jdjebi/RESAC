a07dde574ef6eba0a4a6e78290f641605f08001d
jdjebi <39745200+jdjebi@users.noreply.github.com>
2021-05-27 20:10:13 +0000

    Correctif

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,10 +1,10 @@
 <?php foreach (Flash::get() as $notif): ?>
 <div class="alert alert-<?= $notif["type"] ?> ">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
   <div class="text-center">
     <?= $notif["message"] ?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
   </div>
 </div>
 <?php endforeach;  ?>
