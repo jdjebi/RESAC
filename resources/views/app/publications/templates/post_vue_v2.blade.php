fdae69dd7770d7ff1338cef3719fae7e424eb394
jdjebi <39745200+jdjebi@users.noreply.github.com>
2021-05-27 20:09:43 +0000

    Correction des bugs

--- resources/views/app/publications/templates/post_vue_v2.blade.php
+++ resources/views/app/publications/templates/post_vue_v2.blade.php
@@ -1,5 +1,5 @@
-<div v-for="post in posts" class="pub-box" id="">
-    <div class="box resac-linkedin-shadow bg-white mb-3 resac-clear-rounded-on-sm resac-clear-border-left-on-sm resac-clear-border-right-on-sm">
+<div v-for=" (post,index) in posts" class="pub-box" id="">
+    <div class="box resac-linkedin-shadow bg-white mb-3 pb-1 resac-clear-rounded-on-sm resac-clear-border-left-on-sm resac-clear-border-right-on-sm">
         <div class="header pl-4 pt-3 pb-3 pr-4">
           <div class="media">
             <a v-bind:title:="post.user.fullname" v-bind:href="post.user.profil_url">
@@ -45,5 +45,58 @@
           </div>
         </div>
         <div class="body pl-4 pr-4 pb-3" v-html="post.content"></div>
+        <div class=" pl-4 pr-4">
+
+          <div class="resac-post-option d-flex justify-content-between align-items-center">
+            <div class="small">
+             <a v-on:click="ToggleComment(index)" href="#"><span class="text-muted">@{{ post.comments.length }} commentaires</span></a>
+            </div>
+            <button class="btn btn-sm btn-dark resac-fb-btn-light" v-on:click="ToggleComment(index)">
+              <i class="fa fa-comment-alt"></i>&nbsp; Commenter
+            </button>
+          </div>
+          
+          <div v-show="post.comment.show">
+            <hr class="resac-post-hr-bottom">
+            <div class="resac-post-comment ">
+              <div class="media align-items-center">
+                <img class="border rounded-circle w-20 user-pic" src="{{ photos_cdn_asset(UserAuth()) }}" >
+                <div class="ml-3 media-body">
+                  <form action="{{ route('backend.comments.store') }}" method="POST">
+                    <input type="text" class="comment-input form-control" name="content" placeholder="Commentez puis appuyez entrée...">
+                    <input type="hidden" v-bind:value="post.id" name="post_id">
+                  </form>
+                </div>
+              </div>
+              <div class="text-right">
+                <small class="text-muted">
+                  Enregistement en cours...
+                </small>
+              </div>
+            </div>        
+            <div class="mb-3">
+              <div v-for="comment in post.comments">
+                  <div class="resac-post-comment ">
+                    <div class="media">
+                      <a v-bind:title:="comment.user.fullname" v-bind:href="comment.user.profil_url">
+                        <img class="border rounded-circle w-20 user-pic" v-bind:src="comment.user.photo" v-bind:alt="'Photo' + post.user.fullname">
+                      </a>
+                      <div class="ml-3 media-body">
+                        <div>
+                          <span class="user-name">@{{ comment.user.fullname }}</span> @{{ comment.content }}
+                        </div>
+                        <div class="text-left">
+                          <span class="text-muted small">
+                            <time class="timeago"  v-bind:datetime="comment.created_at" v-bind:title="comment.created_at">@{{ comment.created_at }}</time>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                
+              </div>
+            </div>
+          </div>
+
+        </div>
     </div>
   </div>
